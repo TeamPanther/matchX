f49398bad120b811876f1240a369ac6c569dee98
jiminykbob <jikang92@hotmail.com>
2017-12-08 17:28:10 -0800

    add components for owner

---
+++ client/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserProfile = (props) => {
+  return (
+    <div className='userProfile'>
+      userProfile
+    </div>
+  );
+};
+
+export default UserProfile;
---
+++ client/components/UsersList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import User from './User';
+
+const UsersList = (props) => {
+  return (
+    <div className='usersList'>
+      UsersList
+      <User />
+    </div>
+  );
+};
+
+export default UsersList;
